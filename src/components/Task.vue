17427f86eb9891ba057ea10ea9b0d4a0874dfbb5
Niki <nikitheofilaktidou@gmail.com>
2022-06-13 16:52:23 +0300

    delete  fixed

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -3,8 +3,7 @@
 '', 'task']">
     <h3>
         {{task.text}}
-        <i @click="onDelete(task.id)" class="fas fa-times"></i>
-    </h3>
+        <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>    </h3>
     <p>{{task.day}}</p>
 </div>
 </template>
@@ -24,8 +23,8 @@
 </script>
 
 <style scope>
-.fas{
-    color: red;
+.fas {
+  color: red;
 }
 .task {
   background: #f4f4f4;
@@ -39,6 +38,6 @@
 .task h3 {
   display: flex;
   align-items: center;
-  justify-content: sp
+  justify-content: space-between;
 }
 </style>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,8 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-         <Task @delete-task="$emit('delete-task', task.id)" :task="task"/>
+         <Task 
+         @delete-task="$emit('delete-task', task.id)" 
+         :task="task"/>
     </div>
     
 </template>
@@ -8,12 +10,13 @@
 
 <script>
 import Task from './Task'
+
 export default{
     name: 'Tasks',
     props: {
         tasks: Array,
     },
-    components:{
+    components: {
       Task  
     },
     emits:['delete-task'],
