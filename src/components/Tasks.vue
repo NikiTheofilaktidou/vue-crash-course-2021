d50bc690adce3ff63ccd656d15fe7c2914dba1ed
Niki <nikitheofilaktidou@gmail.com>
2022-06-22 15:41:16 +0300

    conecting this with a web api core project

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -4,13 +4,24 @@
          @toggle-reminder="$emit('toggle-reminder', task.id)"
          @delete-task="$emit('delete-task', task.id)" 
          :task="task"/>
+        
     </div>
+    <div v-for="todo in todoList" :key="todo.Id">
+        <Task
+        @toggle-reminder="$emit('toggle-reminder', todo.id)"
+        @delete-task="$emit('delete-task', todo.id)" 
+        :task="todo"/>
+    </div>
+            
+     
     
 </template>
 
 
 <script>
 import Task from './Task'
+
+import * as axios from 'axios';
 
 export default{
     name: 'Tasks',
@@ -21,5 +32,16 @@ export default{
       Task  
     },
     emits:['delete-task', 'toggle-reminder'],
+        created(){
+            axios
+            .get('http://localhost:5001/api/todoitems')
+            .then(response => this.todoList=response.data)
+        },
+        data() {
+            return {
+                todoList: []
+            }
+        },
 }
+
 </script>
